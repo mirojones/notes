c599839be1506f168febe3d10739c8ad7f961bcd
Miro <hello@miro.boats>
2025-09-29 16:51:48 +1000

    Create service worker based on https://docs.pwabuilder.com/#/home/sw-intro

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
+const CACHE_NAME = 'substack-notes-link-generator-cache';
 
+const PRECACHE_ASSETS = [
+  './favicon.svg',
+  './maskable.svg',
+  './index.html',
+  './site.webmanifest'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll(PRECACHE_ASSETS);
+  })());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // match the request to our cache
+    const cachedResponse = await cache.match(event.request);
+
+    // check if we got a valid response
+    if (cachedResponse !== undefined) {
+      // Cache hit, return the resource
+      return cachedResponse;
+    } else {
+      // Otherwise, go to the network
+      return fetch(event.request);
+    };
+  });
+});
